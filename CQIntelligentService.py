1053e9f25f74c52cf765b54f434cdc4cd45b9189
codezl <56688336+codezl@users.noreply.github.com>
2022-11-15 18:14:45 +0800

    一系列更新

--- CQIntelligentService.py
+++ CQIntelligentService.py
@@ -2,6 +2,7 @@ import socket
 import json
 # 加入词库
 from MysqlConn import conn
+import CQVar
 
 ListenSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 ListenSocket.bind(('127.0.0.1', 5701))
@@ -17,9 +18,9 @@ Content-Type: text/html\r\n\r\n
 
 
 def request_to_json(msg):
-    for i in range(len(msg)):
-        if msg[i] == "{" and msg[-1] == "\n":
-            return json.loads(msg[i:])
+    for ii in range(len(msg)):
+        if msg[ii] == "{" and msg[-1] == "\n":
+            return json.loads(msg[ii:])
     return None
 
 
@@ -64,13 +65,20 @@ def getConn():
     return conn('localhost', 3306, 'root', 'root', 'test')
 
 
+def excuteSql(sql):
+    con = getConn()
+    cur = con.cursor()
+    cur.execute(sql)
+    data = cur.fetchall()
+    return data
+
+
 def getRepFromDB(msg):
     con = getConn()
     cur = con.cursor()
-    cur.execute('SELECT * FROM robot_lexicon WHERE locate(\''+msg+'\',msg) LIMIT 1')
+    cur.execute('SELECT * FROM robot_lexicon WHERE locate(\'' + msg + '\',msg) LIMIT 1')
     data = cur.fetchall()
-    if (len(data) > 0):
-        print(data[0][2])
+    if len(data) > 0:
         res = data[0][2]
     else:
         res = '我不懂呢'
@@ -80,6 +88,16 @@ def getRepFromDB(msg):
     pass
 
 
+# 群聊菜單
+def getGroupMenu():
+    try:
+        menu = excuteSql('select option_index,option_name,id,type,child_id from menu_main')
+        return menu
+    except Exception as e:
+        print(e)
+        return None
+
+
 def createMsg(msgType, number, msg):
     return {'msg_type': msgType, 'number': number, 'msg': msg}
 
@@ -87,10 +105,10 @@ def createMsg(msgType, number, msg):
 while True:
     rev = rev_msg()
     if rev is not None and rev["post_type"] == "message":
-        print('收到消息:'+rev["raw_message"])
+        print('收到消息:' + rev["raw_message"])
         # 从词库中找回答
         repMsg = getRepFromDB(rev['raw_message'])
-        if (repMsg is None):
+        if repMsg is None:
             repMsg = "我不懂呢，还请期待我的学习"
         #     做一些日志记录，后期完善
 
@@ -100,15 +118,39 @@ while True:
             send_msg({'msg_type': 'private', 'number': qq, 'msg': repMsg})
         elif rev["message_type"] == "group":  # 群聊
             group = rev['group_id']
+            if CQVar.IS_MENU:
+                send_msg(createMsg('group', group, '在菜单中'))
+                if '退出菜单' in rev["raw_message"]:
+                    CQVar.IS_MENU = False
+                continue
             if "[CQ:at,qq=3429171731]" in rev["raw_message"]:
                 qq = rev['sender']['user_id']
+                # 测试表情
+                # send_msg(createMsg('group', group, '[CQ:at,qq={}] [CQ:face,id=123]').format(qq))
                 if rev['raw_message'].split(' ')[1] == '在吗':
                     send_msg({'msg_type': 'group', 'number': group, 'msg': '[CQ:poke,qq={}]'.format(qq)})
+                    continue
                 else:
-                    if "[CQ:at,qq=3429171731]" in rev["raw_message"]:
+                    if rev['raw_message'].split(' ')[1] == '':
                         send_msg(createMsg('group', group, '[CQ:at,qq={}]'.format(qq) + ' ' + repMsg))
+                        continue
                     elif "[CQ:poke,qq=3429171731]" in rev["raw_message"]:
                         send_msg(createMsg('group', group, '[CQ:poke,qq={}]'.format(qq)))
+                        continue
+                    elif rev['raw_message'].split(' ')[1] == '菜单':
+                        menuList = '菜单选项:%0a'
+                        menuData = getGroupMenu()
+                        CQVar.NOW_MENU = menuData
+                        if menuData is not None and len(menuData) > 0:
+                            for i in range(len(menuData)):
+                                # %0a是换行的字符编码
+                                menuList = menuList + str(menuData[i][0]) + '.' + menuData[i][1] + '%0a'
+                            send_msg(createMsg('group', group, menuList + '%0a回复数字进入菜单项目,回复 \'退出菜单\'退出'))
+                            CQVar.IS_MENU = True
+                            continue
+                        else:
+                            send_msg(createMsg('group', group, '菜单未创建呢，请先创建菜单'))
+                            continue
                     else:
                         # 其它一些智能指令
                         pass
